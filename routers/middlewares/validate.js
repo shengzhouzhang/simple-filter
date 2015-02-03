53a19049b017a7f563aed59136cc9edf0da7cd96
Steven Zhang <steven.shengzhou@gmail.com>
2015-02-03 20:55:52 +1100

    add validate

--- routers/middlewares/validate.js
+++ routers/middlewares/validate.js
@@ -6,7 +6,7 @@ var validate = function (req, res, next) {
 
   if(!_.isObject(data) ||
      !_.isArray(data.payload)) {
-       
+    
     res.status(400).json({
       error: 'Could not decode request: JSON parsing failed'
     });
