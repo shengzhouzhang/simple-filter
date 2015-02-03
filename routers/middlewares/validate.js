c0cfe3363ad74bcf2010b9cc8e684cfd8ae94e4c
Steven Zhang <steven.shengzhou@gmail.com>
2015-02-03 21:29:04 +1100

    add log

--- routers/middlewares/validate.js
+++ routers/middlewares/validate.js
@@ -4,14 +4,20 @@ const _ = require('lodash');
 var validate = function (req, res, next) {
   var data = req.body;
 
+  console.log('receiving data');
+  console.log(JSON.stringify(data));
+
   if(!_.isObject(data) ||
      !_.isArray(data.payload)) {
-    
+
+    console.error('validate error', !_.isObject(data), !_.isArray(data.payload));
     res.status(400).json({
       error: 'Could not decode request: JSON parsing failed'
     });
     return;
   }
+
+  console.log('validate success');
   next();
 };
 
