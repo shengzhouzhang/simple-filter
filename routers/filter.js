c8c74c1374676043b0fb0132eb2f9b4736f30492
Steven Zhang <steven.shengzhou@gmail.com>
2015-02-03 20:50:52 +1100

    add setheader

--- routers/filter.js
+++ routers/filter.js
@@ -1,31 +1,29 @@
 
 const _ = require('lodash');
 const express = require('express');
+const setHeader = require('./middlewares/setHeader');
 const validate = require('./middlewares/validate');
 const router = express.Router();
 
-var filter = function (req, res, next) {
+var filter = function (req, res) {
   var items = req.body.payload;
 
-  console.log('items', items.length);
   var result = _.chain(items)
     .filter(function (item) {
-      return item.drm && item.episodeCount > 0;
+      return item.drm === true && item.episodeCount > 0;
     })
     .map(function (item) {
       return {
-        image: item.image.showImage,
+        image: item.image ? item.image.showImage : undefined,
         slug: item.slug,
         title: item.title
       };
     })
     .value();
 
-  // console.log('data', data);
-  console.log('result', result);
   res.status(200).json({ response: result });
 };
 
-router.post('/', [ validate ], filter);
+router.post('/', [ setHeader, validate ], filter);
 
 module.exports = router;
--- routers/middlewares/validate.js
+++ routers/middlewares/validate.js
@@ -2,6 +2,16 @@
 const _ = require('lodash');
 
 var validate = function (req, res, next) {
+  var data = req.body;
+
+  if(!_.isObject(data) ||
+     !_.isArray(data.payload)) {
+       
+    res.status(400).json({
+      error: 'Could not decode request: JSON parsing failed'
+    });
+    return;
+  }
   next();
 };
 
